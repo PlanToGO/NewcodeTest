bc09c4387e22802b4a7e91b3216d20ff3d3ecb22
eggleader <qqqq442843239@gmail.com>
2020-08-06 09:17:02 +0800

    Update

--- src/leetcode/fibonacci.java
+++ src/leetcode/fibonacci.java
@@ -1,6 +1,12 @@
 package leetcode;
 
+/**
+ * 题目：斐波那契数列 题号：509
+ * 思路：递归就完事了
+ */
+
 public class fibonacci {
+    //递归的方法，坏处是需要大量的运算，时间复杂度比较高
     public static int fib(int N){
         if (N<=1){
             return N;
@@ -8,9 +14,21 @@ public class fibonacci {
             return fib(N-1)+fib(N-2);
         }
     }
+    //记忆递归的方法，需要额外的空间，但是避免了大量的重复运算
+    public static int fib1(int n){
+        if (n<=1) return n;
+        int[] sum = new int[n+1];
+        sum[0]=0;
+        sum[1]=1;
+        for(int i=2;i< sum.length;i++){
+            sum[i]=sum[i-1]+sum[i-2];
+        }
+        return sum[n];
+    }
+
 
     public static void main(String[] args) {
-        int sum;
-        System.out.println(fib(8));
+        System.out.println(fib(45));
+        System.out.println(fib1(45));
     }
     }
